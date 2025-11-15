c58fcf1d8258e685ac31977011554c690a30673b
jihwan38 <sss203800@gmail.com>
2025-11-15 23:04:08 +0900

    Initial commit: 스프링 프로젝트 초기 설정

---
+++ src/main/java/econovation/moongtaengi/MoongtaengiApplication.java
@@ -0,0 +1,13 @@
+package econovation.moongtaengi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoongtaengiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoongtaengiApplication.class, args);
+	}
+
+}
