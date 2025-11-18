f18e0271f46c8424d38318a9b510be1eeb453b49
이상현 <132208851+sanghyeon1225@users.noreply.github.com>
2025-11-18 17:17:59 +0900

    feat: BaseEntity 구현 (#2)

---
+++ src/main/java/econovation/moongtaengi/global/entity/BaseEntity.java
@@ -0,0 +1,32 @@
+package econovation.moongtaengi.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", updatable = true)
+    private LocalDateTime updatedAt;
+}
