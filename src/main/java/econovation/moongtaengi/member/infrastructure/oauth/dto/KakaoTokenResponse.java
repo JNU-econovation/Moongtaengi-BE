0ff835140bc437af1dda2b37cd734eb97fc01d12
sanghyeon1225 <oopt1234@naver.com>
2025-11-23 14:31:49 +0900

    feat: KakaoTokenResponse에 @JsonNaming 적용

--- src/main/java/econovation/moongtaengi/member/infrastructure/oauth/dto/KakaoTokenResponse.java
+++ src/main/java/econovation/moongtaengi/member/infrastructure/oauth/dto/KakaoTokenResponse.java
@@ -1,24 +1,15 @@
 package econovation.moongtaengi.member.infrastructure.oauth.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoTokenResponse(
-        @JsonProperty("access_token")
         String accessToken,
-
-        @JsonProperty("token_type")
         String tokenType,
-
-        @JsonProperty("refresh_token")
         String refreshToken,
-
-        @JsonProperty("expires_in")
         Integer expiresIn,
-
-        @JsonProperty("scope")
         String scope,
-
-        @JsonProperty("refresh_token_expires_in")
         Integer refreshTokenExpiresIn
 ) {
 }
