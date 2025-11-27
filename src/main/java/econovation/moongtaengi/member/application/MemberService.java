c36c3a8d860356f24ccea6d537e53af766f6ca39
sanghyeon1225 <oopt1234@naver.com>
2025-11-27 16:06:11 +0900

    feat: MemberService에 최종 회원가입 메소드 구현

--- src/main/java/econovation/moongtaengi/member/application/MemberService.java
+++ src/main/java/econovation/moongtaengi/member/application/MemberService.java
@@ -1,15 +1,38 @@
 package econovation.moongtaengi.member.application;
 
+import econovation.moongtaengi.member.domain.Member;
+import econovation.moongtaengi.member.domain.MemberRepository;
+import econovation.moongtaengi.member.domain.Nickname;
 import econovation.moongtaengi.member.domain.NicknameFactory;
+import econovation.moongtaengi.member.exception.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
     private final NicknameFactory nicknameFactory;
 
     public void checkNicknameAvailability(String nickname) {
         nicknameFactory.createNickname(nickname);
+    }
+
+    @Transactional
+    public void completeRegistration(Long memberId, String nicknameValue) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(MemberNotFoundException::new);
+
+        Nickname nickname = nicknameFactory.createNickname(nicknameValue);
+
+        member.completeRegistration(nickname);
+
+        log.info("최종 회원가입 완료 - memberId: {}, nickname: {}",
+                memberId, nickname.getValue());
     }
 }
