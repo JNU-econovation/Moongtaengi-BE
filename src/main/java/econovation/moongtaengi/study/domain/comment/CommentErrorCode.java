6333b23b1622d663bf44ef0388d3bd56ec2f06e2
jihwan38 <sss203800@gmail.com>
2026-01-19 00:58:50 +0900

    feat: 댓글 작성 권한 검증기(Validator) 구현 및 테스트 추가
    - CommentMemberValidator 인터페이스 및 구현체 추가
    - 제출물(Submission)이 속한 스터디의 구성원인지 검증하는 비즈니스 로직 구현
    - 도메인 중립적인 에러 코드 CommentErrorCode.NO_PERMISSION 추가
    - Mockito를 활용한 CommentMemberValidatorImpl 단위 테스트 작성 및 통과

--- src/main/java/econovation/moongtaengi/study/domain/comment/CommentErrorCode.java
+++ src/main/java/econovation/moongtaengi/study/domain/comment/CommentErrorCode.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum CommentErrorCode implements ErrorCode {
     INVALID_COMMENT_INFO(HttpStatus.BAD_REQUEST, "CM_001", "댓글의 필수 정보가 누락되었습니다."),
     CONTENT_TOO_LONG(HttpStatus.BAD_REQUEST, "CM_002", "댓글은 %d자를 초과할 수 없습니다."),
-    NOT_COMMENT_OWNER(HttpStatus.FORBIDDEN, "CM_003", "해당 댓글에 대한 편집 권한이 없습니다.");
+    NOT_COMMENT_OWNER(HttpStatus.FORBIDDEN, "CM_003", "해당 댓글에 대한 편집 권한이 없습니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "CM_004", "존재하지 않는 댓글입니다."),
+    NO_PERMISSION(HttpStatus.FORBIDDEN, "CM_005", "댓글을 작성할 권한이 없습니다.");
 
 
     private final HttpStatus status;
