c0348758604333273cd70a20d3b4769c04dbc2a0
sanghyeon1225 <oopt1234@naver.com>
2026-01-19 02:12:04 +0900

    feat: 멤버 필드 변경에 따른 local sql문 수정

--- src/main/resources/data-local.sql
+++ src/main/resources/data-local.sql
@@ -2,8 +2,8 @@
 DELETE FROM members WHERE kakao_id = 'admin-test';
 
 -- 관리자 계정 INSERT
-INSERT INTO members (id, created_at, updated_at, kakao_id, nickname, role, status)
-VALUES (1, NOW(), NOW(), 'admin-test', '관리자', 'ADMIN', 'ACTIVE');
+INSERT INTO members (id, created_at, updated_at, kakao_id, nickname, role, status, total_experience, profile_icon)
+VALUES (1, NOW(), NOW(), 'admin-test', '관리자', 'ADMIN', 'ACTIVE', 0,'DEFAULT');
 
 -- ✅ AUTO_INCREMENT를 2부터 시작
 ALTER TABLE members ALTER COLUMN id RESTART WITH 2;
