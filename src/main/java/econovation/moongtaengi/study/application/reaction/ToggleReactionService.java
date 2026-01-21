e563536a403fceb28360f8ee2e983fcd281b8fd2
sanghyeon1225 <oopt1234@naver.com>
2026-01-21 21:46:19 +0900

    feat: 감정표현 추가 시 이벤트 발행

--- src/main/java/econovation/moongtaengi/study/application/reaction/ToggleReactionService.java
+++ src/main/java/econovation/moongtaengi/study/application/reaction/ToggleReactionService.java
@@ -2,11 +2,13 @@ package econovation.moongtaengi.study.application.reaction;
 
 import econovation.moongtaengi.study.domain.reaction.EmojiType;
 import econovation.moongtaengi.study.domain.reaction.Reaction;
+import econovation.moongtaengi.study.domain.reaction.ReactionAddedEvent;
 import econovation.moongtaengi.study.domain.reaction.ReactionMemberValidator;
 import econovation.moongtaengi.study.domain.reaction.ReactionRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,7 @@ public class ToggleReactionService {
 
     private final ReactionRepository reactionRepository;
     private final ReactionMemberValidator reactionMemberValidator;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public void toggleReaction(Long submissionId, Long memberId, EmojiType emojiType) {
@@ -33,6 +36,14 @@ public class ToggleReactionService {
         } else {
             Reaction newReaction = Reaction.create(submissionId, memberId, emojiType);
             reactionRepository.save(newReaction);
+
+            // 감정표현 추가 이벤트 발행
+            eventPublisher.publishEvent(new ReactionAddedEvent(
+                    newReaction.getId(),
+                    submissionId,
+                    memberId,
+                    emojiType
+            ));
 
             log.info("감정표현 추가 성공 - submissionId: {}, memberId: {}, type: {}",
                     submissionId, memberId, emojiType);
